8fc33d622c0368418608d1b9cba2582f040e593d
William-Vigo <17559768+William-Vigo@users.noreply.github.com>
2021-02-08 03:17:55 -0500

    created new script to clone directory

---
+++ scripts/clone-plugins.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+echo "cloning powerlevel10k"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "cloning zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
